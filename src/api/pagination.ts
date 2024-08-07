d5b4d263171d3ead955f08157ad6bee3fd9cb1b3
ErikonMACC <erik.roblox0602@gmail.com>
2024-08-07 19:48:29 -0300

    Cleaning up the file structure

--- src/api/pagination.ts
+++
@@ -1,7 +0,0 @@
-
-export interface Pagination<T> {
-  currentPage: number,
-  items: number,
-  pages: number,
-  rows: T
-}
--- src/app.tsx
+++ src/app.tsx
@@ -3,12 +3,11 @@
 // import { Toaster } from 'sonner'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './lib/react-query'
-import { Navbar } from './components/Navbar'
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Navbar />
+
     </QueryClientProvider>
   )
 }
